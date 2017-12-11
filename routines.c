42d5d0d105b3ae32e94dbb3a3efdad69316253a1
mxflknbrg <32801318+mxflknbrg@users.noreply.github.com>
2017-12-11 19:21:27 +0100

    Add files via upload

--- routines.c
+++ routines.c
@@ -47,59 +47,78 @@ void TIMER_carrierFreq_init(void)
 }
 
 
+
+/*
+ * Generates a pulse or a "pause" (sets a low on the output pin)
+ * with half of the IRQ frequency
+ */
 ISR(TIMER1_COMPA_vect)
 {
 	//PINB |= (1 << PB7);
 
-
 	if(flag_pulse)
 	{
-		PINB |= (1 << PB7);
+		PINB |= (1 << PB7);		//Toggle output pin
 		//PORTB ^= (1 << PB7);
-		cnt_pulse++;
-		flag_pulse = 0;
+		cnt_pulse++;			//Inc pulse counter
 	}
+
 	if(flag_pause)
 	{
-		PORTB &= ~(1 << PB7);
-		cnt_pause++;
-		flag_pause = 0;
+		PORTB &= ~(1 << PB7);	//Low on output pin
+		cnt_pause++;			//Inc pause counter
 	}
 
+	flag_pulse = 0;				//Reset flags
+	flag_pause = 0;
 }
 
-
+/*
+ * Generates a NEC coded logical 1 modulated on a 38 kHz carrier frequency
+ * The logical 1 consists of 21 pulses and a pause equivalent to 64 pulses
+ */
 static inline void generate_one()
 {
 	while(cnt_pulse < TICKS_PULSE)
 	{
 		flag_pulse = 1;
 	}
+	cnt_pulse = 0;
+
 	while(cnt_pause < TICKS_1_PAUSE)
 	{
 		flag_pause = 1;
 	}
+	cnt_pause = 0;
 }
 
+
+/*
+ * Generates a NEC coded logical 0 modulated on a 38 kHz carrier frequency
+ * The logical 0 consists of 21 pulses an d a pause equivalent to 21 pulses
+ */
 static inline void generate_zero()
 {
 	while(cnt_pulse < TICKS_PULSE)
 	{
 		flag_pulse = 1;
 	}
+	cnt_pulse = 0;
+
 	while(cnt_pause < TICKS_0_PAUSE)
 	{
 		flag_pause = 1;
 	}
+	cnt_pause = 0;
 }
 
 
-void command_hex(uint8_t command)
+void NEC_out(uint8_t command_hex)
 {
 	uint8_t val = 0x80;
-	for(uint8_t i = 0; i < 8; i ++)
+	for(uint8_t i = 0; i < 8; i++)
 	{
-		if(command & val)
+		if(command_hex & val)
 		{
 			generate_one();
 		}
